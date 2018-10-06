066f7c647f91c6ad16b98dcb4e7eb1541e88b985
VentGrey <omarpurataf@gmail.com>
2018-10-06 10:31:50 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dragon-vpn
+
+Este es un fork de la VPN libre y gratuita alojada en vpn.lastdragon.net
+la funcionalidad es la misma, el repositorio solo sirve para estilar la
+página para hacerla un poco mas "amigable" con el usuario.
---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html class="no-js" lang="es-MX">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>VPN Libre y gratutia para navegacion anonima de Last Dragon</title>
+</head>
+
+
+<table style="text-align: left; width: 100%;" border="1"
+ cellpadding="2" cellspacing="2">
+  <tbody>
+    <tr>
+      <td style="text-align: center;"><a href="index.php">Inicio / Home</a></td>
+      <td style="text-align: center;"><a href="quees.php">¿ Que es este sitio ?</a></td>
+      <td style="text-align: center;"><a href="descargar.php">Descargar software y Configuración</a></td>
+      <td style="text-align: center;"></td>
+      <td style="text-align: center;"></td>
+      <td style="text-align: center;"></td>
+    </tr>
+  </tbody>
+</table>
+
+<p>
+
+<center>
+<script type="text/javascript">
+    google_ad_client = "ca-pub-5748342367700253";
+    google_ad_slot = "3730532167";
+    google_ad_width = 320;
+    google_ad_height = 100;
+</script>
+<!-- Banner -->
+<script type="text/javascript"
+src="https://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+</center>
+
+
+</p>
+
+
+
+<table style="text-align: left; width: 100%;" border="1"
+ cellpadding="2" cellspacing="2">
+  <tbody>
+    <tr style="color: white;" align="center">
+      <td style="background-color: black;">Tu IP es:</td>
+    </tr>
+    <tr align="center">
+      <td>Tu IP Real: 177.228.33.12 <br> Tu IP de navegacion: <iframe src="https://www.lastdragon.net/ip.php" frameborder="0" height="40" width="300"></iframe></td>
+    </tr>
+  </tbody>
+</table>
+
+<p>
+<center>
+<form method="POST" action="https://vpn.lastdragon.net/genuser.php">
+<input type="submit" value="Generar usuario">
+</form>
+</center>
+</p>
+
+
+<p>
+<center>
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Dragonminimalista -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:600px"
+     data-ad-client="ca-pub-5748342367700253"
+     data-ad-slot="6992968566"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+
+</center>
+</p>
+
+
+</html>
+
+
