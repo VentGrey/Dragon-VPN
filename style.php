024317054acea6119230cdff0ce6ddd4b15960ee
VentGrey <omarpurataf@gmail.com>
2018-10-06 19:48:47 -0500

    active link rework

--- style.php
+++ style.php
@@ -14,6 +14,14 @@ body {
 }
 
 /* Estilar los enlaces dentro de la barra de navegacion */
+
+
+/* Coloreado de los enlaces una vez se pase el cursor encima de éstos */
+.navegacion a:hover a:focus a:active {
+    color: #999;
+    text-decoration:none;
+}
+
 .navegacion a {
     font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif;
     text-decoration: none;
@@ -34,9 +42,26 @@ body {
     color: #000;
 }
 
-/* Coloreado de los enlaces una vez se pase el cursor encima de éstos */
-.navegacion a:hover a:focus a:active {
-    color: #999;
-    text-decoration:none;
+.navegacion a::before {
+    content: '';
+    display: block;
+    position: absolute;
+    bottom: 3px;
+    left: 0;
+    height: 3px;
+    width = 100%;
+    background-color: #000;
+    transform-origin: right top;
+    transform: scale(0, 1);
+    transition: color 0.1s, transform 0.2s ease out;
 }
 
+
+.navegacion a:active::before {
+    background-color: #000;
+}
+
+.navegacion a:hover::before, a:focus::before {
+    transform-origin: left top;
+    transform: scale(1, 1);
+}
