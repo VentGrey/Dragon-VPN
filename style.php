c9b68fa664ae2430a6455e3fc22dd99c5861807c
VentGrey <omarpurataf@gmail.com>
2018-10-07 00:36:51 -0500

    Final links version

--- style.php
+++ style.php
@@ -7,65 +7,37 @@ body {
     background: #FAFAFA;
 }
 
-/* Color de fondo para la barra de navegación superior */
+/* Color de fondo para la barra de navegación */
+
 .navegacion {
-    background-color: #272A31;
+    background-color:#FFFFFF;
     overflow: hidden;
 }
 
-/* Estilar los enlaces dentro de la barra de navegacion */
-
-
-/* Coloreado de los enlaces una vez se pase el cursor encima de éstos */
-.navegacion a:hover a:focus a:active {
-    color: #999;
-    text-decoration:none;
-}
+/* Colores bonitos a los links */
 
 .navegacion a {
-    font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif;
+    float: left;
+    display: block;
+    color: #212121;
+    text-align: center;
+    padding: 14px 16px;
     text-decoration: none;
-    transition: color 0.1s, background-color 0.1s;
+    font-size: 18px;
+    border-bottom: 3px solid transparent;
+    font-family: Helvetica, Sans-Serif;
 }
 
-.navegacion a {
-    position: relative;
-    display: block;
-    padding: 16px 0;
-    margin: 0 12px;
-    letter-spacing: 1px;
-    font-size: 12px;
-    line-height: 16px;
-    font-weight: 900;
-    text-transform: uppercase;
-    transition: color 0.1s, background-color 0.1s, padding 0.2s ease-in;
-    color: #000;
+/* Decoración cuando se ponga el cursor sobre un link */
+
+.navegacion a:hover {
+    border-bottom: 3px solid #0F6EE3;
 }
 
-.navegacion a::before {
-    content: '';
-    display: block;
-    position: absolute;
-    bottom: 3px;
-    left: 0;
-    height: 3px;
-    width = 100%;
-    background-color: #000;
-    transform-origin: right top;
-    transform: scale(0, 1);
-    transition: color 0.1s, transform 0.2s ease out;
+/* Decoración cuando un link sea seleccionado */
+.navegacion a.activo {
+    border-bottom: 3px solid #0F6EE3;
 }
-
-
-.navegacion a:active::before {
-    background-color: #000;
-}
-
-.navegacion a:hover::before, a:focus::before {
-    transform-origin: left top;
-    transform: scale(1, 1);
-}
-
 
 /* Estilado de la dirección IP */
 .direccion td {
